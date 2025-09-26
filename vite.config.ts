fafa23a2d0e4649f87837d1a71e3c472d97006ce
laicq <1280060812@qq.com>
2025-09-26 18:10:35 +0800

    pack

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 import { visualizer } from 'rollup-plugin-visualizer'
 import monacoEditorPlugin from 'vite-plugin-monaco-editor'
 // 代码压缩
-// import viteCompression from 'vite-plugin-compression'
+import viteCompression from 'vite-plugin-compression'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,17 +20,17 @@ export default defineConfig({
       languageWorkers: ['editorWorkerService', 'css', 'html', 'json', 'typescript'],
       // 其他插件选项...
     }),
-    // viteCompression({
-    // 压缩代码
-    // algorithm: 'brotliCompress', // 压缩算法，可选['gzip', 'brotliCompress', 'deflate', 'deflateRaw']
-    // ext: '.gz', // 压缩文件的扩展名
-    // threshold: 10240, // 文件大小超过10240字节（10KB）时才进行压缩
-    // deleteOriginFile: false, // 压缩后是否删除源文件
-    // verbose: true, // 是否在控制台输出压缩结果，默认为true
-    // disable: false, // 是否禁用压缩，默认为false
-    // filter: /\.(js|css|html)$/i, // 指定需要压缩的文件类型
-    // compressionOptions: {}, // 压缩算法的参数
-    // }),
+    viteCompression({
+      // 压缩代码
+      algorithm: 'gzip', // 压缩算法，可选['gzip', 'brotliCompress', 'deflate', 'deflateRaw']
+      ext: '.gz', // 压缩文件的扩展名
+      threshold: 10240, // 文件大小超过10240字节（10KB）时才进行压缩
+      deleteOriginFile: false, // 压缩后是否删除源文件
+      // verbose: true, // 是否在控制台输出压缩结果，默认为true
+      // disable: false, // 是否禁用压缩，默认为false
+      // filter: /\.(js|css|html)$/i, // 指定需要压缩的文件类型
+      // compressionOptions: {}, // 压缩算法的参数
+    }),
     visualizer({
       gzipSize: true, // 显示各文件在经过 gzip 压缩后的大小
       brotliSize: true, // 显示各文件在经过 brotli 压缩后的
